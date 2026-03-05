84b01c25b3dd36608c5f793c2b1b5534a8e94129
FM <fm@MacBookPro.lan>
2026-03-05 01:43:43 +0100

    Remove unused files and cleanup project structure

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.dirname(__file__))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_Portfolio.production_settings')
-
-from my_Portfolio.wsgi import application
